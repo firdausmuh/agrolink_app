d23e005b27800389c58c4f9767dd13d163d37587
firdausmuh <muhfrds345@gmai.com>
2024-12-12 02:35:51 +0700

    add arrow-back at riwayat transaksi

--- lib/screens/transaksi/riwayat_transaksi_screen.dart
+++ lib/screens/transaksi/riwayat_transaksi_screen.dart
@@ -5,69 +5,157 @@ class RiwayatTransaksiScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const SizedBox(
-            height: 20,
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white, // Warna AppBar
+        elevation: 0, // Menghilangkan bayangan AppBar
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black), // Ikon panah kembali
+          onPressed: () {
+            Navigator.pop(context); // Aksi kembali ke halaman sebelumnya
+          },
+        ),
+        title: const Text(
+          'Riwayat Transaksi',
+          style: TextStyle(
+            fontWeight: FontWeight.w700,
+            fontSize: 20,
+            color: Colors.black,
           ),
-          const Text(
-            'Transaksi',
-            style: TextStyle(
-              fontWeight: FontWeight.w700,
-              fontSize: 20,
+        ),
+        centerTitle: false,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 20,
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          const Expanded(
-            child: SingleChildScrollView(
-              physics: BouncingScrollPhysics(),
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 20,
-                  ),
-                ],
+            const Expanded(
+              child: SingleChildScrollView(
+                physics: BouncingScrollPhysics(),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 20,
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text('Total', style: TextStyle(
-                color: Color(0xFF406A52),
-                fontWeight: FontWeight.w700,
-                fontSize: 24,
-              ),),
-              Text('Rp. 40.000', style: TextStyle(
-                color: Color(0xFF406A52),
-                fontWeight: FontWeight.w700,
-                fontSize: 24,
-              ),),
-            ],
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          SizedBox(
-            width: MediaQuery.of(context).size.width,
-            child: ElevatedButton(onPressed: () {}, style: ElevatedButton.styleFrom(
-              backgroundColor: Color(0xFF406A52),
-
-            ), child: Text('Pesan Sekarang', style: TextStyle(color: Colors.white),)),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-        ],
+            const SizedBox(
+              height: 20,
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Total',
+                  style: TextStyle(
+                    color: Color(0xFF406A52),
+                    fontWeight: FontWeight.w700,
+                    fontSize: 24,
+                  ),
+                ),
+                Text(
+                  'Rp. 40.000',
+                  style: TextStyle(
+                    color: Color(0xFF406A52),
+                    fontWeight: FontWeight.w700,
+                    fontSize: 24,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFF406A52),
+                ),
+                child: const Text(
+                  'Pesan Sekarang',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
       ),
     );
+    // return Padding(
+    //   padding: const EdgeInsets.symmetric(horizontal: 20.0),
+    //   child: Column(
+    //     crossAxisAlignment: CrossAxisAlignment.start,
+    //     children: [
+    //       const SizedBox(
+    //         height: 20,
+    //       ),
+    //
+    //       const Text(
+    //         'Riwayat Transaksi',
+    //         style: TextStyle(
+    //           fontWeight: FontWeight.w700,
+    //           fontSize: 20,
+    //         ),
+    //       ),
+    //       const SizedBox(
+    //         height: 20,
+    //       ),
+    //       const Expanded(
+    //         child: SingleChildScrollView(
+    //           physics: BouncingScrollPhysics(),
+    //           child: Column(
+    //             children: [
+    //               SizedBox(
+    //                 height: 20,
+    //               ),
+    //             ],
+    //           ),
+    //         ),
+    //       ),
+    //       const SizedBox(
+    //         height: 20,
+    //       ),
+    //       const Row(
+    //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    //         children: [
+    //           Text('Total', style: TextStyle(
+    //             color: Color(0xFF406A52),
+    //             fontWeight: FontWeight.w700,
+    //             fontSize: 24,
+    //           ),),
+    //           Text('Rp. 40.000', style: TextStyle(
+    //             color: Color(0xFF406A52),
+    //             fontWeight: FontWeight.w700,
+    //             fontSize: 24,
+    //           ),),
+    //         ],
+    //       ),
+    //       const SizedBox(
+    //         height: 20,
+    //       ),
+    //       SizedBox(
+    //         width: MediaQuery.of(context).size.width,
+    //         child: ElevatedButton(onPressed: () {}, style: ElevatedButton.styleFrom(
+    //           backgroundColor: Color(0xFF406A52),
+    //
+    //         ), child: Text('Pesan Sekarang', style: TextStyle(color: Colors.white),)),
+    //       ),
+    //       const SizedBox(
+    //         height: 20,
+    //       ),
+    //     ],
+    //   ),
+    // );
   }
 }
